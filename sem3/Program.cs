09629c80cff98adc34376c993dd71684dad3522b
nimat <niymet.p@ya.ru>
2022-08-16 22:01:50 +0300

    2:00

--- sem3/Program.cs
+++ sem3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void PrintQuater(int x, int y)
+{
+    if (x>0 && y>0)
+    {
+        Console.WriteLine("Первая четверть");
+    }
+    else if (x<0 && y>0)
+    {
+        Console.WriteLine("Вторая четверть");
+    }
+    else if (x>0 && y<0) 
+    {
+        Console.WriteLine("Третья четверть");
+    }
+    else if (x<0 && y<0)
+    {
+        Console.WriteLine("Четвертая четверть");
+    }
+    else
+    {
+        Console.WriteLine("Определить четверть не удалось");
+    }
+}
+
+
+Console.WriteLine("Введите координату X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+PrintQuater(x, y);
