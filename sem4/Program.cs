660a42a16ba727c0f95b7a5edbe71cb9122c4172
nimat <niymet.p@ya.ru>
2022-08-19 17:36:33 +0300

    all is cool

--- sem4/Program.cs
+++ sem4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от 1 до {number}: {Method(number)}");
+
+int Method(int num){
+    int sum = 0;
+    for (int i=1; i<=number; i++) 
+    {
+        sum += i;
+    }
+
+    // int i=1;
+    // int sum = 0;
+    // do 
+    // {
+    //     sum +=i;
+    //     i++;
+    // } 
+    // while (i<=number);
+
+    return sum;
+}
