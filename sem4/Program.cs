e6a58322478c9165843daed18455743194248e27
nimat <niymet.p@ya.ru>
2022-08-19 21:36:01 +0300

    finish

--- sem4/Program.cs
+++ sem4/Program.cs
@@ -20,3 +20,30 @@ int Method(int num){
 
     return sum;
 }
+
+Console.WriteLine($"Количество цифр в числе {number}: {Method2(number)}");
+
+int Method2(int num)
+{
+    int k = 0;
+    do
+    {
+        number = number/10;
+        k++;
+        }
+    while (number>9);
+    return k+1;
+}
+
+Method3();
+
+void Method3()
+{
+    int[] arr = new int[8];
+    for(int i=0; i<arr.Length; i++)
+    {
+        Random rnd = new Random();
+        arr[i] = rnd.Next(0,2);
+        Console.Write($" {arr[i]} ");
+    }
+}
